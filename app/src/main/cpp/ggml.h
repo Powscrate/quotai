fdd19cfac85bc8020c384d6484f4186ef1e159cb
Powscrate <tantelygasikara@gmail.com>
2026-06-01 17:22:38 +0300

    Adding missing files v3

--- app/src/main/cpp/ggml.h
+++ app/src/main/cpp/ggml.h
@@ -219,6 +219,14 @@
 #define GGML_QNT_VERSION        2    // bump this on quantization format changes
 #define GGML_QNT_VERSION_FACTOR 1000 // do not change this
 
+#ifndef GGML_VERSION
+#define GGML_VERSION "unknown"
+#endif
+
+#ifndef GGML_COMMIT
+#define GGML_COMMIT "unknown"
+#endif
+
 #define GGML_MAX_DIMS           4
 #define GGML_MAX_PARAMS         2048
 #define GGML_MAX_SRC            10
